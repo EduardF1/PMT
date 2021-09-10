b2f324242f58b32077531364775153a09c04e78a
EduardF1 <fischer_eduard@yahoo.com>
2021-09-10 17:19:57 +0200

    add custom validation error class, handler and reponse

--- src/main/java/com/edfis/ppmtool/services/ProjectService.java
+++ src/main/java/com/edfis/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.edfis.ppmtool.services;
 
 import com.edfis.ppmtool.domain.Project;
+import com.edfis.ppmtool.exceptions.ProjectIdException;
 import com.edfis.ppmtool.repositories.IProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,12 @@ public class ProjectService {
     @Autowired
     private IProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+    public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 }
