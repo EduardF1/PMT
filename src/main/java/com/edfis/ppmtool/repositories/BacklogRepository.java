de9b489def8fd1374eefd1d58594ed5e19073c96
EduardF1 <fischer_eduard@yahoo.com>
2021-09-11 20:52:29 +0200

    add models and repositories for backlog and projectTask

---
+++ src/main/java/com/edfis/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package com.edfis.ppmtool.repositories;
+
+import com.edfis.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+}
---
+++ src/main/java/com/edfis/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,7 @@
+package com.edfis.ppmtool.repositories;
+
+import com.edfis.ppmtool.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
