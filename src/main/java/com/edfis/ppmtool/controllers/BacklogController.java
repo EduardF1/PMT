94617ec367ed9871dd7172f03e412be9db565612
EduardF1 <fischer_eduard@yahoo.com>
2021-09-11 23:05:41 +0200

    initial implementation for Backlog controller and PT service

---
+++ src/main/java/com/edfis/ppmtool/controllers/BacklogController.java
@@ -0,0 +1,35 @@
+package com.edfis.ppmtool.controllers;
+
+import com.edfis.ppmtool.domain.ProjectTask;
+import com.edfis.ppmtool.services.ProjectTaskService;
+import com.edfis.ppmtool.services.ValidationErrorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/backlog")
+public class BacklogController {
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private ValidationErrorService validationErrorService;
+
+    @PostMapping("/{backlogId}")
+    public ResponseEntity<?> addProjectTaskToBacklog(@Valid @RequestBody ProjectTask projectTask,
+                                                     BindingResult result,
+                                                     @PathVariable String backlogId){
+        ResponseEntity<?> errorMap = validationErrorService.validate(result);
+        if(errorMap != null) return errorMap;
+
+        ProjectTask newProjectTask = projectTaskService.addProjectTask(backlogId, projectTask);
+        return new ResponseEntity<>(newProjectTask, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/edfis/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,37 @@
+package com.edfis.ppmtool.services;
+
+import com.edfis.ppmtool.domain.Backlog;
+import com.edfis.ppmtool.domain.ProjectTask;
+import com.edfis.ppmtool.repositories.BacklogRepository;
+import com.edfis.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
+        Integer backlogPTSequence = backlog.getPTSequence();
+        backlogPTSequence++;
+        backlog.setPTSequence(backlogPTSequence);
+        projectTask.setProjectSequence(projectIdentifier+"-"+backlogPTSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+        if (projectTask.getPriority() == null) {
+            projectTask.setPriority(3);
+        }
+
+        if (projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+}
